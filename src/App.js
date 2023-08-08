428e571d3fcbff0df23cab455ffd17bc6fab5550
reshad21 <miakala7@gmail.com>
2023-08-08 13:19:57 +0600

    store add to the app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Provider } from 'react-redux';
 import { RouterProvider } from "react-router-dom";
+import { store } from './redux/app/store';
 import routes from "./routes/routes";
-
 function App() {
   return (
     <>
+    <Provider store={store}>
       <RouterProvider router={routes} />
+    </Provider>
     </>
   );
 }
